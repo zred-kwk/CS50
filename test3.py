23e0b8b79c2c2d8afa2b576d49e4a6dd46deb97a
zred-kwk <75919316+zred-kwk@users.noreply.github.com>
2025-04-24 17:29:07 +0800

    first commit for this project

--- test3.py
+++ test3.py
@@ -1,11 +1,1 @@
-def my_decorator(func):
-    def wrapper(*args, **kwargs):
-        print("Calling function with arguments:", args, kwargs)
-        return func(*args, **kwargs)
-    return wrapper
-
-@my_decorator
-def greet(name):
-    print(f"Hello, {name}!")
-
-greet('wekiat')
+check50 --help
